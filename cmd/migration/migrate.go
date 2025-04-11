ecba52d55a976a7bac005a350673893b6a77ba3e
aliffian.rizki <aliffian.rizki@rakhasa.com>
2025-04-11 10:31:58 +0700

    feat: soal terakhir

--- cmd/migration/migrate.go
+++ cmd/migration/migrate.go
@@ -1,5 +1,18 @@
 package migration
 
+import (
+	"technicaltest/config"
+	"technicaltest/database"
+	"technicaltest/database/entities"
+)
+
 func main() {
+	config.NewConfig()
+	database.ConnectDB()
+	entities := []any{&entities.MyClient{}}
+
+	if err := database.DB.AutoMigrate(entities); err != nil {
+		panic(err)
+	}
 
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,5 +1,27 @@
 package server
 
-func main() {
+import (
+	"technicaltest/config"
+	"technicaltest/database"
+	"technicaltest/handler"
+	"technicaltest/repository"
+	"technicaltest/service"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.NewConfig()
+	database.ConnectDB()
+
+	r := gin.Default()
+	handler := handler.NewHandler(service.NewService(repository.NewRepository(database.DB)))
+
+	r.POST("/create-client", handler.Create)
+	r.GET("", handler.GetAll)
+	r.GET("/:id", handler.GetByID)
+	r.PUT("/:id", handler.Update)
+	r.DELETE("/:id", handler.Delete)
+
+	r.Run()
 }
